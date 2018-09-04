bd1a77a34399091ef5b3372c38185d5501297299
snpr <vaugk13@gmail.com>
2018-09-04 09:21:16 +0500

    refactoring

--- js/stat.js
+++ js/stat.js
@@ -5,13 +5,11 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 110;
 var CLOUD_Y = 20;
 var GAP = 10;
-var TEXT_Y_GAP = 230;
+var TEXT_Y_GAP = 235;
 var TEXT_X_GAP = 90;
 var FONT_GAP = 5;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
-var BAR_X = 155;
-var BAR_Y = 85;
 
 var renderCloud = function (x, y, width, height, color, ctx) {
   ctx.fillStyle = color;
@@ -30,15 +28,12 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     var maxTime = Math.max.apply({}, times);
     var proportion = maxTime / BAR_HEIGHT;
-
     ctx.fillText(names[i], CLOUD_X + GAP * 2 + TEXT_X_GAP * i, GAP * 2 + TEXT_Y_GAP);
-    ctx.fillStyle = 'rgba(255, ' + Math.random() * 255 + ', 100)';
-
-    ctx.fillRect(CLOUD_X + GAP * 2 + TEXT_X_GAP * i, BAR_Y - FONT_GAP + BAR_X, BAR_WIDTH, -(times[i] / proportion));
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * 2 + TEXT_X_GAP * i, TEXT_Y_GAP - FONT_GAP - (times[i] / proportion));
+    ctx.fillStyle = 'rgb(255, ' + Math.random() * 255 + ', 100)';
+    ctx.fillRect(CLOUD_X + GAP * 2 + TEXT_X_GAP * i, TEXT_Y_GAP, BAR_WIDTH, -(times[i] / proportion));
     ctx.font = '16px PT Mono';
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * 2 + TEXT_X_GAP * i, BAR_Y - FONT_GAP + BAR_HEIGHT - (times[i] / proportion));
   }
-
 };
 
